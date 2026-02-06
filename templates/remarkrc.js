7467ef05c5e8a1dc8d7d06a06d6a1f8ce74d8ef1
toki83 <toki83w@gmail.com>
2026-02-06 22:55:39 +0100

    Table of contents auto generated

--- templates/remarkrc.js
+++ templates/remarkrc.js
@@ -6,7 +6,9 @@ import remarkGithubAlerts from "remark-github-alerts";
 import remarkMath from "remark-math";
 import remarkAsciimath from "remark-asciimath";
 import remarkKroki from "remark-kroki";
+import remarkFlexibleToc from "remark-flexible-toc";
 import remarkRehype from "remark-rehype";
+import rehypeSlug from "rehype-slug";
 import rehypeKatex from "rehype-katex";
 import rehypeDocument from "rehype-document";
 import rehypeStringify from "rehype-stringify";
@@ -34,7 +36,17 @@ export default {
     remarkMath,
     remarkAsciimath,
     [remarkKroki, { server: "https://kroki.io", output: "inline-svg" }],
+    // Recognizes [toc] and builds the list
+    [
+      remarkFlexibleToc,
+      {
+        placeholder: "[toc]",
+        containerTag: "nav",
+        containerClassName: "table-of-contents",
+      },
+    ],
     [remarkRehype, { allowDangerousHtml: true }],
+    rehypeSlug, // Generates <h2 id="my-heading"> for the TOC to link to
     rehypeKatex,
     [
       rehypeDocument,
@@ -47,7 +59,14 @@ export default {
           },
         ],
         css: ["https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css"],
-        style: cssContent,
+        style:
+          cssContent +
+          `
+            html { scroll-behavior: smooth; }
+            .table-of-contents ul { list-style-type: none; padding-left: 1.5rem; }
+            .table-of-contents a { text-decoration: none; color: #58a6ff; }
+            .table-of-contents a:hover { text-decoration: underline; }
+        `,
         script: `
           const pid = window.location.pathname.split('_').pop().split('.')[0];
           const scrollFile = 'scroll_' + pid + '.json';
